3d584944853e6f4739eb9175913e367e8945ef5f
MaruiMisato <219howagucho@gmail.com>
2017-11-15 23:15:07 +0900

    1.0.1
    行数削減

--- Support-for-translation-of-thesis.py
+++ Support-for-translation-of-thesis.py
@@ -1,28 +1,22 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 15 17:52:33 2017
 
-@author: kanri
-"""
 import pyperclip
 import webbrowser
-txt = pyperclip.paste()
-print('text data:\n', txt)
 
-#Delete all newline characters
-txt= txt.replace('\n',' ')
+txt = pyperclip.paste()
+print('txt data:\n', txt)
+
+txt= txt.replace('\n',' ')#Delete all newline characters
 txt = txt.replace('\r','')
 
-#New line for each sentence
-txt = txt.replace('.',".\n")
-txt = txt.replace("\n ",'\n')
+txt = txt.replace('. ',".\n")#New line for each sentence
+
 txt = txt.replace("ig.\n","ig.")#Fig. n
 txt = txt.replace("i.\ne.\n","i.e.")#i.e.
 txt = txt.replace("et al.\n","et al.")#et al.
-#n.m
 for n in "0123456789":
     for m in "0123456789":
-        txt = txt.replace(n+".\n"+m,n+'.'+m)
+        txt = txt.replace(n+".\n"+m,n+'.'+m)#n.m
         
 pyperclip.copy(txt)
 print('replaced text:\n',txt)
